963dd1fea5c22c722dd6e54d1271fbdbc3b23824
Nidhin <nidhintsajee@gmail.com>
2016-08-09 12:07:30 +0530

    testing

--- 10.py
+++ 10.py
@@ -2,16 +2,15 @@
 
 import test2
 
-total_tests = 0
-tests_passed = 0
+total_tests = passed_tests = 0
 
 for symbol in dir(test2):
     attr = getattr(test2, symbol)
     if (callable(attr) and attr.__name__.startswith('test')):
         if attr():
-            tests_passed += 1
+            passed_tests += 1
         total_tests += 1
 
-print 'Executed %d tests' % total_tests
-print 'Total tests passed = %d' % tests_passed
+print 'Total executed tests : '+ str(total_tests)
+print 'Total passed tests : '+ str(tests_passed)
 
